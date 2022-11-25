dca2dae2938c21b258fcb459228ccba624c303a2
João Ricardo Viana <joaorviana98@gmail.com>
2022-11-25 15:48:05 -0300

    Arrumando a vitoria e a derrota

--- script.js
+++ script.js
@@ -13,7 +13,26 @@ const randomNumber = ()=>{
     const number = Math.round((Math.random()*251)+1)
     return number.toString()
 }
-
+const cleanScreen = ()=>{
+    answer = []
+    attempts = 3
+    correctAnswer = false
+    const son = document.querySelector('#gameContainer')
+    game.removeChild(son)
+    title.style.display = ''
+    menuContainer.style.display = 'flex'
+    body.style.background =' url("imagens/forest.jpg") no-repeat'
+    body.style.backgroundSize = 'cover'
+}
+const win =()=>{
+    const button = document.querySelector('#buttonAnswer')
+    button.innerHTML = 'Parabéns você acertou'
+}
+const loose = ()=>{
+    const button = document.querySelector('#buttonAnswer')
+    button.innerHTML = `Você errou mais sorte da próxima vez
+    o pokemon era o: ${word}`
+}
 const getPokemon = async(pokemonNumber)=>{
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`)
     const data = await APIResponse.json()
@@ -25,8 +44,10 @@ const pokemonRender = async(pokemonNumber)=>{
     const img = document.createElement('img')
     img.src = data.sprites.versions['generation-v']['black-white'].animated.front_default
     img.className = 'pokemon'
-    body.style.background =' url("imagens/deepForest.png")'
-    body.style.backgroundSize = '100%'
+    img.style.filter = 'brightness(0%)'
+    body.style.background =' url("imagens/deepForest.png") no-repeat'
+    body.style.backgroundSize = 'cover'
+    body.style.height = '100vh'
     gameContainer.appendChild(img)
     title.style.display = 'none'
     menuContainer.style.display = 'none'
@@ -47,22 +68,21 @@ const cleanTextArea = () =>{
         paragrafs[i].value = ''
     }
 }
-const endGame = ()=>{
-    if(correctAnswer === true || attempts <1){
-        answer = []
-        attempts = 3
-        const son = document.querySelector('#gameContainer')
-        game.removeChild(son)
-        title.style.display = ''
-        menuContainer.style.display = 'flex'
-        body.style.background =' url("imagens/forest.jpg") no-repeat'
-        body.style.backgroundSize = '100%'
+const endGame =  async()=>{
+    const buttonAnswer = document.querySelector('#buttonAnswer')
+    buttonAnswer.disabled = true
+    const pokemon = document.querySelector('.pokemon')
+    pokemon.style.filter = 'brightness(100%)'
+    if( correctAnswer === true){
+        win()
+    }else{
+        loose()
     }
+    setTimeout(cleanScreen,3000)
 }
 
 const sendAnswer = ()=>{
     const paragrafs = document.querySelectorAll('.line')
-    console.log(paragrafs)
     for(let i = 0;i<paragrafs.length ;i+=1){
         answer.push(paragrafs[i].value)
     }
@@ -77,7 +97,9 @@ const sendAnswer = ()=>{
         attempts -=1
         cleanTextArea()
     }
-    endGame()
+    if(correctAnswer === true || attempts <1){
+        endGame()
+    }
 }
 
 const secretWord = async() =>{
@@ -87,6 +109,7 @@ const secretWord = async() =>{
     word = await pokemonRender(randomNumber())
     let secretWord = []
     const lettersContainer = document.createElement('div')
+    
     gameContainer.appendChild(lettersContainer)
     lettersContainer.style.display ='flex'
     lettersContainer.style.justifyContent = 'center'
@@ -98,7 +121,7 @@ const secretWord = async() =>{
         line.addEventListener('keyup',catchValues)
     }
     const button = document.createElement('button')
-    button.className = 'buttonAnswer'
+    button.id = 'buttonAnswer'
     button.innerHTML = 'Enviar Resposta'
     button.addEventListener('click',sendAnswer)
     gameContainer.appendChild(button)
